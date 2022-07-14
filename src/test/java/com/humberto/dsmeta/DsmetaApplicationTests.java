602e98367270355e2b448dc45ae3d6c7c0f9765b
José Humberto <jhpnascimento96@gmail.com>
2022-07-14 08:50:04 -0300

    Projected Created

---
+++ src/test/java/com/humberto/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humberto.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
